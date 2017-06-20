33897a54bcef3eb5b6dcb01e5ee605aece1d8341
Atanas9708 <nasko_9708@mail.bg>
2017-06-20 15:48:16 +0300

    Adding Homework

---
+++ JS Fundamentals/01. Lab Intro to JavaScript/1. Sum 3 Numbers.js
@@ -0,0 +1,5 @@
+function sum(a, b ,c ) {
+    console.log(a + b + c);
+}
+
+sum(2, 3, 4)
---
+++ JS Fundamentals/02. Lab Control-Flow Logic/2. Boxes and Bottles.js
@@ -0,0 +1,7 @@
+function boxes(a, b) {
+
+   console.log(Math.ceil(a / b));
+}
+boxes(20,5);
+boxes(15,7);
+boxes(5,10);
